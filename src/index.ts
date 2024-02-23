46f761b90957f2a5a46793fb7da21d5271bc33a6
tallyto <rodrigues.tallyto@hotmail.com>
2024-02-23 19:05:16 -0300

    feat: configurado cors e swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import fastify from 'fastify'
 import 'reflect-metadata';
+import fastify from 'fastify'
 import fjwt from '@fastify/jwt'
-import {UserRoutes} from './app/routes/UserRoutes';
-import {AppDataSource} from './data-source';
-import {BrandRoutes} from './app/routes/BrandRoutes';
-import {InventoryRoutes} from './app/routes/InventoryRoutes';
-import {ProductRoutes} from './app/routes/ProductRoutes';
+import swagger from '@fastify/swagger';
+import swaggerui from '@fastify/swagger-ui';
+import cors from '@fastify/cors'
+
+import { UserRoutes } from './app/routes/UserRoutes';
+import { AppDataSource } from './data-source';
+import { BrandRoutes } from './app/routes/BrandRoutes';
+import { InventoryRoutes } from './app/routes/InventoryRoutes';
+import { ProductRoutes } from './app/routes/ProductRoutes';
+import userSchema from './app/schemas/UserSchema';
+import brandSchema from './app/schemas/BrandSchema';
+import inventorySchema from './app/schemas/InventorySchema';
+import productSchema from './app/schemas/ProductSchema';
 
 const server = fastify()
 
@@ -15,16 +23,66 @@ const app = async () => {
     try {
         await AppDataSource.initialize()
 
+        server.register(cors, {
+            allowedHeaders: "*",
+            origin: "*",
+            methods: "*",
+        })
+
+        server.register(swagger, {
+            swagger: {
+                info: {
+                    title: 'Oboticário Challenge',
+                    description: 'API para cadastro de produtos',
+                    version: '0.1.0'
+                },
+                externalDocs: {
+                    url: 'https://github.com/tallyto/oboticrud',
+                    description: 'Mais informações aqui'
+                },
+                schemes: ['http'],
+                consumes: ['application/json'],
+                produces: ['application/json'],
+                tags: [
+                    { name: 'user', description: 'User related end-points' },
+                ],
+                definitions: {
+                    User: userSchema,
+                    Brand: brandSchema,
+                    Inventory: inventorySchema,
+                    Product: productSchema
+                },
+                securityDefinitions: {
+                    bearerAuth: {
+                        type: 'apiKey',
+                        name: 'Authorization',
+                        in: 'header',
+                        description: 'JWT Bearer token',
+                    },
+                },
+                security: [
+                    {
+                        bearerAuth: [],
+                    },
+                ],
+            }
+        })
+
+        server.register(swaggerui, {
+            routePrefix: '/documentation',
+
+        })
+
         server.register(fjwt, {
             secret: 'oboticrud-challenge'
         })
 
         server.get('/ping', (req, res) => {
-            res.send({message: "pong"})
+            res.send({ message: "pong" })
         })
 
         server.get('/', (req, res) => {
-            res.send({message: "Hello World"})
+            res.send({ message: "Hello World" })
         })
 
         server.register(UserRoutes, {
@@ -46,7 +104,7 @@ const app = async () => {
 
         const PORT = Number(process.env.PORT) || 3000
 
-        server.listen({port: PORT}, (err, address) => {
+        server.listen({ port: PORT }, (err, address) => {
             if (err) {
                 console.error(err)
                 process.exit(1)
